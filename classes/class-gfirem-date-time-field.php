790445d25ef3b24175d915f1f6da0fefcef0f463
Victor Marin <marin250189@gmail.com>
2018-03-19 21:50:03 -0400

    Problema de css: DateTimePicker Inline Mode

--- classes/class-gfirem-date-time-field.php
+++ classes/class-gfirem-date-time-field.php
@@ -64,7 +64,7 @@ class GFireMDateTimeField extends GFireMFieldBase {
 		$base_url = GFireMDateTime::$assets;
 		wp_enqueue_style( 'jquery.datetimepicker', $base_url . 'css/jquery.datetimepicker.min.css', array(), $this->version );
         wp_enqueue_script( 'DateFormatter', $base_url . 'js/php-date-formatter.js', array( "jquery" ), $this->version, true );
-		wp_enqueue_script( 'jquery.datetimepicker', $base_url . 'js/jquery.datetimepicker.full.min.js', array( 'jquery' ), $this->version, true );
+		wp_enqueue_script( 'jquery.datetimepicker', $base_url . 'js/jquery.datetimepicker.full.js', array( 'jquery' ), $this->version, true );
 		wp_enqueue_script( 'date_time_field', $base_url . 'js/date_time_field.js', array( 'jquery' ), $this->version, true );
 		$params = array(
 			'now_date' => date( 'Y/m/d' ),
